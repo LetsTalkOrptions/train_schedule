2fd8fe81fcda0b3e00c80011a7f55fd189337cf1
jcress <cresswellj@outlook.com>
2018-08-13 15:55:47 -0400

    reformatted page to match colors

--- assets/java/app.js
+++ assets/java/app.js
@@ -1,4 +1,4 @@
- // Initialize Firebase
+// Initialize Firebase
 var config = {
     apiKey: "AIzaSyDboCQrnQ8A6Me56I3kpz-i9tR6wDR6R7A",
     authDomain: "first-project-b815a.firebaseapp.com",
@@ -10,7 +10,7 @@ var config = {
 
 firebase.initializeApp(config);
 
-
+// set a variable to the database
 var database = firebase.database();
 
 function updateTime() {
@@ -20,7 +20,7 @@ function updateTime() {
 updateTime();
 
 
-// 2. Button for adding Employees
+// button for adding trains
 $(".btn").on("click", function (event) {
     event.preventDefault();
 
@@ -41,12 +41,7 @@ $(".btn").on("click", function (event) {
     // Uploads employee data to the database
     database.ref().push(newTrain);
 
-    // // Logs everything to console
-    // console.log(newTrain.trainName);
-    // console.log(newTrain.destination);
-    // console.log(newTrain.firstTrainTime);
-    // console.log(newTrain.frequency);
-
+    // alert user a new train was added
     alert("New Train successfully added");
 
     // Clears all of the text-boxes
@@ -56,15 +51,15 @@ $(".btn").on("click", function (event) {
     $("#frequency").val("");
 });
 
-database.ref().on("child_added", function(childSnapshot) {
+database.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val());
-  
+
     // Store everything into a variable.
     var trainNameInput = childSnapshot.val().trainName;
     var destinationInput = childSnapshot.val().destination;
     var firstTrainTimeInput = childSnapshot.val().firstTrainTime;
     var frequencyInput = childSnapshot.val().frequency;
-  
+
     // Train Info
     console.log(trainNameInput);
     console.log(destinationInput);
@@ -74,41 +69,42 @@ database.ref().on("child_added", function(childSnapshot) {
     // time calculations
     var currentTime = moment().format("HH:mm");
     startTime = moment(firstTrainTime);
-    var timeDifference = moment()-moment(firstTrainTimeInput, "HH:mm");
-    var minutesDifference = ((timeDifference/1000)/60);
-    var remainder = (minutesDifference%frequencyInput);
-    var minutesTil = (frequencyInput-remainder);
+    var timeDifference = moment() - moment(firstTrainTimeInput, "HH:mm");
+    var minutesDifference = ((timeDifference / 1000) / 60);
+    var remainder = (minutesDifference % frequencyInput);
+    var minutesTil = (frequencyInput - remainder);
     setInterval
-    
+
+    // log time calculations
     console.log("Current time is: " + currentTime);
     console.log("Start time was: " + firstTrainTimeInput);
     console.log("Time Difference in minutes: " + minutesDifference);
     console.log("Minutes until: " + minutesTil);
 
-    
+
 
 
     // train arrival calculation
     var trainArrival = moment().add(minutesTil, "m").format("HH:mm");
     console.log("Next train arrives at " + trainArrival)
-    
+
     // time until next train arrives
     var remainderMinutes = (Math.round(minutesTil));
     console.log("Remainder: " + remainderMinutes);
 
-    
 
-  // Create the new row
-  var newRow = $("<tr>").append(
-    $("<td>").text(trainNameInput),
-    $("<td>").text(destinationInput),
-    $("<td id='onTime'>").text("ON TIME"),
-    $("<td>").text(trainArrival),
-    $("<td>").text(remainderMinutes),
-    $("<td>").text(frequencyInput)
-  );
 
-  // Append the new row to the table
-  $(".table > tbody").append(newRow);
+    // Create the new row
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainNameInput),
+        $("<td>").text(destinationInput),
+        $("<td id='onTime'>").text("ON TIME"),
+        $("<td>").text(trainArrival),
+        $("<td>").text(remainderMinutes),
+        $("<td>").text(frequencyInput)
+    );
+
+    // Append the new row to the table
+    $(".table > tbody").append(newRow);
 
 })
